5ca023ac6be2939dc1c798fc0bc2995e66687d1f
Michael Zhang <heronalps@gmail.com>
2018-12-11 18:59:32 -0800

    mz - modified build.sh

--- site/build.sh
+++ site/build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-cd Temp_Predictor/site
+cd ~/Temp_Predictor/site
 virtualenv venv --python=python3
 source venv/bin/activate
 pip install -r ./requirements.txt
 
 current_path=$PWD
-path="$PWD/venv/lib/python3.6/site-packages"
+path1="$PWD/venv/lib64/python3.6/dist-packages"
+path2="$PWD/venv/lib/python3.6/dist-packages"
 rm -f lambda_function.zip
-zip -r9 lambda_function.zip ./predictor/predictor.py 
+zip -rj9 lambda_function.zip ./predictor/predictor.py 
 
-cd $path
-echo $PWD
+cd $path1
 zip -ur $current_path/lambda_function.zip numpy/ matplotlib/ pandas/ scipy/ sklearn/
+
+cd $path2
+zip -ur $current_path/lambda_function.zip pyparsing.py six.py pytz/
+
 cd $current_path
 echo 'Complete packaging lambda function'
+
+aws s3api put-object --bucket temp-predictor --key lambda_function.zip --body lambda_function.zip
+echo 'Uploaded to s3 bucket'
